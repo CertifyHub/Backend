fff7dd179a0a14c457661349eb4e974490f27f4a
gagle1231 <kke1231@naver.com>
2024-03-27 20:45:25 +0900

    first commit

---
+++ src/main/java/com/cert/certihub/CertiHubApplication.java
@@ -0,0 +1,13 @@
+package com.cert.certihub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertiHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CertiHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cert/certihub/CertiHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cert.certihub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertiHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
